96250501e0ea36eb1b4b1dc62dac3b14578206a8
Adam Hoyle <adam@adamhoyle.co.uk>
2017-11-29 23:12:58 +0000

    improved logging

--- step.sh
+++ step.sh
@@ -5,5 +5,5 @@ set -e
 
 which diawi >/dev/null 2>&1 || npm install -g diawi
 diawi_url=$(diawi upload "${api_token}" "${filename}")
-echo Uploaded to Diawi, URL is $diawi_url
+echo Diawi Upload URL is $diawi_url
 envman add --key DIAWI_UPLOAD_URL --value "${diawi_url}"
